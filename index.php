556cdd7391c14a9d2dc786b6676323be4d02c5fc
Phill <phillip@myth.co.nz>
2015-02-23 11:20:14 +1300

    initial

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include('templates/head.html'); ?>
+
+	<body class="home-page">
+
+<?php include('templates/svg.html'); ?>
+
+<?php include('templates/header.php'); ?>
+
+		<section id="main" role="main">
+			<div class="l-section">
+
+			</div>
+		</section>
+
+<?php include('templates/footer.html'); ?>
+
+<?php include('templates/footer-includes.html'); ?>
+
+	</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,13 @@
+		<section id="header" role="banner">
+
+			<header class="l-section">
+				<h1>
+					<a class="logo" href="index.php">
+						<span rel="logo">Site Name</span>
+					</a>
+				</h1>
+			</header>
+
+<?php include('templates/navigation.html'); ?>
+
+		</section>
